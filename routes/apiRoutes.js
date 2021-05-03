1a0b41e693178c7c8823c8a2f7b0f688d36b012d
F34r Th3 R34per <danielpisani12@hotmail.com>
2021-05-03 16:39:14 -0400

    initial setup of files. node_modules were accidentally included. Will be later removed.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+
+const noteData = require('../db/db.json');
+
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => res.json(noteData));
+    app.post('/api/notes', (req, res) => noteData.push(req.body));
+
+};
